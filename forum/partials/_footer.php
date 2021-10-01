951b09c135b3caef4853ba48b83c2f26ed3ab87b
Shruti Sharma <86105293+shruti325@users.noreply.github.com>
2021-10-01 23:52:37 +0530

    Update _footer.php

--- forum/partials/_footer.php
+++ forum/partials/_footer.php
@@ -1,6 +1,5 @@
 <?php
-echo '<div class="container-fluid bg-dark text-light">
-  <p class="text-center mb-0">
+echo '<div class="container-fluid bg-dark text-light fixed-bottom">
         Developed by master coder Vishwas
     </p>
 </div>'; 
