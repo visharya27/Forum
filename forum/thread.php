08d911a2eac6e4241e089cf0b44c90f6de1e559e
Shruti Sharma <86105293+shruti325@users.noreply.github.com>
2021-10-02 21:36:03 +0530

    Update thread.php

--- forum/thread.php
+++ forum/thread.php
@@ -19,3 +19,48 @@
 
 </head>
 
+<body>
+    <?php include 'partials/_header.php';?>
+    <?php include 'partials/_dbconnect.php';?>
+
+    <?php
+    $id = $_GET['threadid'];
+    $sql = "SELECT * FROM `threads` WHERE thread_id=$id"; 
+    $result = mysqli_query($conn, $sql);
+    while($row = mysqli_fetch_assoc($result)){
+        $title = $row['thread_title'];
+        $desc = $row['thread_desc'];}
+
+?>
+
+    <div class="container my-4">
+        <div class="jumbotron">
+            <h1 class="display-4"> <?php echo $title; ?></h1>
+            <p class="lead"><?php echo $desc;?></p>
+            <hr class="my-4">
+            <p>This is a peer to peer forum.</p>
+            <p>Posted by user</p>
+        </div>
+
+    </div>
+    <div class="container py-2">
+        <h1>Discussion</h1>
+       
+    </div> -->
+    <!-- Optional JavaScript; choose one of the two! -->
+
+    <!-- Option 1: Bootstrap Bundle with Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-/bQdsTh/da6pkI1MST/rWKFNjaCP5gBSY4sEBT38Q/9RBh9AH40zEOg7Hlq2THRZ" crossorigin="anonymous">
+    </script>
+
+    <!-- Option 2: Separate Popper and Bootstrap JS -->
+    <!--
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.3/dist/umd/popper.min.js" integrity="sha384-W8fXfP3gkOKtndU4JGtKDvXbO53Wy8SZCQHczT5FMiiqmQfUpWbYdTil/SxwZgAN" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/js/bootstrap.min.js" integrity="sha384-skAcpIdS7UcVUC05LJ9Dxay8AXcDYfBJqt1CJ85S/CFujBsIzCIv+l9liuYLaMQ/" crossorigin="anonymous"></script>
+    -->
+    <?php include 'partials/_footer.php';?>
+</body>
+
+</html>
+
